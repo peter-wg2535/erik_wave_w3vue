5d530a6f3c9320006da70f2387fa7e249e9354a2
technqvi <38780060+technqvi@users.noreply.github.com>
2022-06-20 16:44:40 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useCryptoStore } from './store/crypto'
+
+const props = useCryptoStore()
+
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Pongtorn Sangkaphet Project',
+  meta: [
+    { name: 'description', content: 'Crypto Project' },
+  ],
+})
+
+</script>
+
+<template>
+  {{ props.loading }}
+  <Loader v-if="props.loading" />
+  <router-view />
+</template>
